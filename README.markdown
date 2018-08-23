9718aab664bea037aa0312f63e3b3d680a88c372
Maksim Fedotov <m.a.fedotov@gmail.com>
2018-08-23 23:16:30 +0300

    add ability to split config into 2 files

--- README.markdown
+++ README.markdown
@@ -226,6 +226,14 @@ key utl for the yumrepo-resource in RedHat-based systems, defaults to 'https://w
 ##### `override_config_settings`
 Which configuration variables should be overriden. Hash, defaults to {} (empty hash).
 
+##### `split_config`
+If set, ProxySQL config file will be split in 2: main config file with admin and mysql variables and proxy config file with servers\users\hostgroups\scheduler\rules params. Defaults to false
+
+##### `proxy_config_file`
+The file where servers\users\hostgroups\scheduler\rules params of ProxySQL configuration are saved. This will only be configured if `split_config` is set to `true`. Defaults to 'proxysql_proxy.cnf'
+
+#####`manage_proxy_config_file`
+Determines wheter this module will update the ProxySQL proxy configuration file. Defaults to 'true'
 
 ## Types
 #### proxy_global_variable
--- manifests/init.pp
+++ manifests/init.pp
@@ -110,6 +110,17 @@
 # * `override_config_settings`
 #   Which configuration variables should be overriden. Hash, defaults to {} (empty hash).
 #
+# * `split_config`
+#   If set, ProxySQL config file will be split in 2: main config file with admin and mysql variables
+#   and proxy config file with servers\users\hostgroups\scheduler params. Defaults to false
+#
+# * `proxy_config_file`
+#   The file where servers\users\hostgroups\scheduler\rules params of ProxySQL configuration are saved
+#   This will only be configured if `split_config` is set to `true`. Defaults to 'proxysql_proxy.cnf'
+#
+# * `manage_proxy_config_file`
+#   Determines wheter this module will update the ProxySQL proxy configuration file. Defaults to 'true'
+#
 class proxysql (
   String $package_name = $::proxysql::params::package_name,
   String $package_ensure = $::proxysql::params::package_ensure,
@@ -132,9 +143,12 @@ class proxysql (
   String $monitor_username = $::proxysql::params::monitor_username,
   Sensitive[String] $monitor_password = $::proxysql::params::monitor_password,
 
+  Boolean $split_config = $::proxysql::params::split_config,
   String $config_file = $::proxysql::params::config_file,
   Boolean $manage_config_file = $::proxysql::params::manage_config_file,
-
+  String $proxy_config_file = $::proxysql::params::proxy_config_file,
+  Boolean $manage_proxy_config_file = $::proxysql::params::manage_proxy_config_file,
+  
   String $mycnf_file_name = $::proxysql::params::mycnf_file_name,
   Boolean $manage_mycnf_file = $::proxysql::params::manage_mycnf_file,
 
